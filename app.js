b57269a29dcf8a4e75748f1bdf449cbab85095e8
Daniil Smyshlyaev <smyshlyeavar@gmail.com>
2020-10-21 23:26:17 +0300

    feat: added function hasDigitPassword

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let authUserData = null;
+let userDatabase = [];
+
 function isValidEmail(email) {
   let regularExpressionForCheckingMail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (regularExpressionForCheckingMail.test(email) === false) {
@@ -5,18 +8,26 @@ function isValidEmail(email) {
   }
   return true;
 }
-function isValidPassword(password){
+
+function hasDigitPassword(password) {
   let digits = "0123456789";
   let isDigit = false;
-  for(let i = 0; i < password.length; i++){
-    if(digits.indexOf(password[i] != -1)){
+  for (let i = 0; i < password.length; i++) {
+    if (digits.indexOf(password[i] !== -1)) {
       isDigit = true;
     }
   }
-  if(password.length >= 6 &&
-    isDigit &&
-    password.charAt(0).toUpperCase() === password.charAt(0)){
+  return isDigit;
+}
+
+function isValidPassword(password) {
+  if (
+    password.length >= 6 &&
+    hasDigitPassword(password) &&
+    password.charAt(0).toUpperCase() === password.charAt(0)
+  ) {
     return true;
   }
   return false;
 }
+
