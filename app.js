3051c407ef676b0f2c209baf9a4571b55760ad28
Daniil Smyshlyaev <smyshlyeavar@gmail.com>
2020-10-21 23:17:21 +0300

    fix: rename function checkingValidateMail in isValidEmail

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function checkigValidateMail(email) {
+function isValidEmail(email) {
   let regularExpressionForCheckingMail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (regularExpressionForCheckingMail.test(email) === false) {
     return false;
